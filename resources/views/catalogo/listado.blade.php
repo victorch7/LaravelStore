eb864ff19cef44e29ebe5080909ad8371b56a3d4
Javi <jhonxavi830@gmail.com>
2024-01-10 21:11:20 -0500

    Agregando Catalogo

--- resources/views/catalogo/listado.blade.php
+++ resources/views/catalogo/listado.blade.php
@@ -8,339 +8,196 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
-<body>
-
-    <section class="products">
-        <h3>Catálogo de Productos</h3><br>
-
-        <h4>Camisas</h4>
-		<div class="all-products">
-			<div class="product">
-				<img src="public\images\1702079321-camisaSco.jpg">
-				<div class="product-info">
-					<h4 class="product-title">Scorpions
-					</h4>
-					<p class="product-price">$54.900</p>
-					<a class="product-btn" href="#">Buy Now</a>
-
-				</div>
-			</div>
-			<div class="product">
-				<img src="public\images\1702080636-camisaRammstein.jpg">
-				<div class="product-info">
-					<h4 class="product-title">Rammstein
-					</h4>
-					<p class="product-price">$54.900</p>
-					<a class="product-btn" href="#">Buy Now</a>
-
-				</div>
-			</div>
-			<div class="product">
-				<img src="public\images\camisaZepelin.jpg">
-				<div class="product-info">
-					<h4 class="product-title">Zeppelin
-					</h4>
-					<p class="product-price">$54.900</p>
-					<a class="product-btn" href="#">Buy Now</a>
-
-				</div>
-			</div>
-			<div class="product">
-				<img src="public\images\camisaRamones.jpg">
-				<div class="product-info">
-					<h4 class="product-title">Ramones
-						</h4>
-					<p class="product-price">$54.900</p>
-					<a class="product-btn" href="#">Buy Now</a>
-
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<section class="products">
-        <h4>Esqueletos</h4>
-		<div class="all-products">
-			<div class="product">
-				<img src="public\images\blusaPantera.jpg">
-				<div class="product-info">
-					<h4 class="product-title">Pantera
-					</h4>
-					<p class="product-price">$39.000</p>
-					<a class="product-btn" href="#">Buy Now</a>
-
-				</div>
-			</div>
-			<div class="product">
-				<img src="public\images\blusaSlayer.jpg">
-				<div class="product-info">
-					<h4 class="product-title">Slayer
-					</h4>
-					<p class="product-price">$39.000</p>
-					<a class="product-btn" href="#">Buy Now</a>
-
-				</div>
-			</div>
-			<div class="product">
-				<img src="public\images\blusaSoda.jpg">
-				<div class="product-info">
-					<h4 class="product-title">Soda Stereo
-					</h4>
-					<p class="product-price">$39.000
-                    </p>
-					<a class="product-btn" href="#">Buy Now</a>
-
-				</div>
-			</div>
-			<div class="product">
-				<img src="public\images\camisaRamones.jpg">
-				<div class="product-info">
-					<h4 class="product-title">Ramones
-						</h4>
-					<p class="product-price">$54.900</p>
-					<a class="product-btn" href="#">Buy Now</a>
-
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<section class="products">
-        <h4>Gorras & Gorros</h4>
-		<div class="all-products">
-			<div class="product">
-				<img src="public\images\gorraKiss.jpg">
-				<div class="product-info">
-					<h4 class="product-title">Kiss
-					</h4>
-					<p class="product-price">$26.900</p>
-					<a class="product-btn" href="#">Buy Now</a>
-
-				</div>
-			</div>
-			<div class="product">
-				<img src="public\images\gorraKra.jpg">
-				<div class="product-info">
-					<h4 class="product-title">Kraken
-					</h4>
-					<p class="product-price">$26.900</p>
-					<a class="product-btn" href="#">Buy Now</a>
-
-				</div>
-			</div>
-			<div class="product">
-				<img src="public\images\gorro1.jpg">
-				<div class="product-info">
-					<h4 class="product-title">Metalica
-					</h4>
-					<p class="product-price">$28.000
-                    </p>
-					<a class="product-btn" href="#">Buy Now</a>
-
-				</div>
-			</div>
-			<div class="product">
-				<img src="public\images\gorro2.jpg">
-				<div class="product-info">
-					<h4 class="product-title">Héroes del Silencio
-						</h4>
-					<p class="product-price">$28.000</p>
-					<a class="product-btn" href="#">Buy Now</a>
-
-				</div>
-			</div>
-		</div>
-    </section>
-
-    <section class="products" style="display: none;">
-        
-        <div class="all-products">
-            <div class="product">
-                <img src="public\images\blusaPantera.jpg">
-                <div class="product-info">
-                    <h4 class="product-title">Pantera
-                    </h4>
-                    <p class="product-price">$39.000</p>
-                    <a class="product-btn" href="#">Buy Now</a>
-
-                </div>
-            </div>
-            <!-- Agrega más productos de esqueletos si es necesario -->
-        </div>
-    </section>
-
-    <section class="products" style="display: none;">
-        <h4>Gorras & Gorros</h4>
-        <div class="all-products">
-            <div class="product">
-                <img src="public\images\gorraKiss.jpg">
-                <div class="product-info">
-                    <h4 class="product-title">Kiss
-                    </h4>
-                    <p class="product-price">$26.900</p>
-                    <a class="product-btn" href="#">Buy Now</a>
-
-                </div>
-            </div>
-            <!-- Agrega más productos de gorros y gorras si es necesario -->
-        </div>
-    </section>
-
-    <nav aria-label="Page navigation example" class="d-flex justify-content-center">
-        <ul class="pagination">
-            <li class="page-item">
-                <a class="page-link" href="#" aria-label="Previous" style="color: tomato; background-color: black; border: 1px solid tomato;">
-                    <span aria-hidden="true">&laquo;</span>
-                </a>
-            </li>
-            <li class="page-item"><a class="page-link" href="#" style="color: black; background-color: tomato; border: 1px solid tomato;">1</a></li>
-            <li class="page-item"><a class="page-link" href="#" style="color: black; background-color: tomato; border: 1px solid tomato;">2</a></li>
-            <li class="page-item"><a class="page-link" href="#" style="color: black; background-color: tomato; border: 1px solid tomato;">3</a></li>
-            <li class="page-item">
-                <a class="page-link" href="#" aria-label="Next" style="color: tomato; background-color: black; border: 1px solid tomato;">
-                    <span aria-hidden="true">&raquo;</span>
-                </a>
-            </li>
-        </ul>
-    </nav>
-
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.min.js"></script>
-
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            var paginationLinks = document.querySelectorAll('.pagination .page-link');
-            var prevButton = document.querySelector('.pagination .page-prev');
-            var nextButton = document.querySelector('.pagination .page-next');
-
-            paginationLinks.forEach(function (link, index) {
-                link.addEventListener('click', function (event) {
-                    event.preventDefault();
-                    var pageNumber = index + 0;
-                    if (pageNumber >= 1 && pageNumber <= 3) {
-                        showPage(pageNumber);
-                    }
-                });
-            });
-
-            prevButton.addEventListener('click', function (event) {
-                event.preventDefault();
-                var currentPage = getCurrentPage();
-                var prevPage = currentPage - 1;
-                if (prevPage >= 1) {
-                    showPage(prevPage);
-                }
-            });
-
-            nextButton.addEventListener('click', function (event) {
-                event.preventDefault();
-                var currentPage = getCurrentPage();
-                var nextPage = currentPage + 1;
-                if (nextPage <= 3) {
-                    showPage(nextPage);
-                }
-            });
-
-            function showPage(pageNumber) {
-                var allSections = document.querySelectorAll('.products');
-                allSections.forEach(function (section, index) {
-                    if (index + 1 === pageNumber) {
-                        section.style.display = 'block';
-                    } else {
-                        section.style.display = 'none';
-                    }
-                });
-            }
-
-            function getCurrentPage() {
-                var visibleSection = document.querySelector('.products[style="display: block;"]');
-                var currentPage = Array.from(document.querySelectorAll('.products')).indexOf(visibleSection) + 1;
-                return currentPage;
-            }
-        });
-    </script>
-
-</body>
-
-</html>
-
-
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Merriweather+Sans&display=swap');
+    body {
+        background-color: #0a0a0a;
+        margin: 0;
+        padding: 0;
+        font-family: 'Arial', sans-serif;
+    }
 
-*{
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-}
+    .navbar {
+        background-color: #333333;
+        padding: 10px 20px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
 
-body{
-	font-size: 1.7rem;
-	font-family: 'Merriweather Sans', sans-serif;
-	background: black;
-	color: #ececec;
-	padding: 4rem;
-}
+    .logo {
+        color: #ff4500;
+        font-size: 24px;
+        font-family: 'Rock Salt', cursive;
+        text-transform: uppercase;
+        text-decoration: none;
+    }
 
-h3{
-	text-align: center;
-}
+    .search-form {
+        display: flex;
+        align-items: center;
+    }
 
-.all-products{
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	flex-wrap: wrap;
-}
+    .search-input {
+        padding: 8px;
+        margin-right: 10px;
+        border: 1px solid #ff4500;
+        border-radius: 5px;
+        color: #ff4500;
+        background-color: #333333;
+        font-family: 'Arial', sans-serif;
+    }
 
-.product{
-	overflow: hidden;
-	background: tomato;
-	color: #21201e;
-	text-align: center;
-	width: 240px;
-	height: 400px;
-	padding: 2rem;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	border-radius: 1.2rem;
-	margin: 2rem;
-}
+    .search-button {
+        background-color: #ff4500;
+        color: #000000;
+        border: 2px solid #ff4500;
+        border-radius: 5px;
+        padding: 8px 15px;
+        cursor: pointer;
+        transition: background-color 0.3s ease-in-out;
+        text-transform: uppercase;
+    }
 
-.product .product-title, .product .product-price{
-	font-size: 18px;
-}
+    .search-button:hover {
+        background-color: #000000;
+        color: #ff4500;
+    }
 
-.product:hover img{
-	scale:  1.1;
-}
+    .container {
+        color: #ffffff;
+        padding: 20px;
+        text-align: center;
+    }
 
-.product:hover {
-	box-shadow: 5px 15px 25px #eeeeee;
-}
+    .rock-title {
+        font-size: 42px;
+        font-family: 'Rock Salt', cursive;
+        color: #ff4500;
+        text-transform: uppercase;
+        letter-spacing: 3px;
+        margin-bottom: 30px;
+    }
 
-.product img {
-	height: 200px;
-	margin: 1rem;
-	transition: all 0.3s;
-}
+    .card {
+        background-color: #333333;
+        color: #ffffff;
+        border: 2px solid #000000;
+        border-radius: 10px;
+        margin-bottom: 20px;
+        width: 25rem;
+        display: inline-block;
+        margin-right: 10px;
+        position: relative;
+        overflow: hidden;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+        transition: box-shadow 0.3s ease-in-out;
+    }
 
-.product a:link, .product a:visited{
-	color: #ececec;
-	display: inline-block;
-	text-decoration: none;
-	background-color: #2c3e50;
-	padding: 1.2rem 3rem;
-	border-radius: 1rem;
-	margin-top: 1rem;
-	font-size: 14px;
-	transition: all 0.2s;
-}
+    .card:hover {
+        box-shadow: 0 8px 16px rgba(0, 0, 0, 0.5);
+    }
 
-.product a:hover{
-	transform: scale(1.1);
-}
+    .card img {
+        height: 300px; 
+        object-fit: cover;
+        border-bottom: 1px solid #000000;
+        width: 100%;
+        transition: transform 0.3s ease-in-out;
+    }
+
+    .larger-image {
+        display: none;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        max-width: 95%;
+        max-height: 95%;
+        z-index: 1;
+    }
+
+    .card:hover img {
+        transform: scale(1.1);
+    }
+
+    .card-body {
+        padding: 15px;
+    }
+
+    .card-title {
+        color: #ff4500;
+        font-size: 20px;
+        margin-top: 10px;
+        text-transform: uppercase;
+    }
+
+    .card-text {
+        color: #cccccc;
+        text-transform: uppercase;
+    }
+
+    .card-description {
+        margin-bottom: 10px;
+    }
+
+    .card-price {
+        font-size: 20px;
+        font-weight: bold;
+        text-transform: uppercase;
+        color: #ff4500;
+    }
+
+    button {
+        background-color: #ff4500;
+        color: #000000;
+        border: 2px solid #ff4500;
+        border-radius: 5px;
+        padding: 8px 15px;
+        cursor: pointer;
+        transition: background-color 0.3s ease-in-out;
+        text-transform: uppercase;
+    }
+
+    button:hover {
+        background-color: #000000;
+        color: #ff4500;
+    }
 </style>
+
+<div class="navbar">
+    <a href="#" class="logo">Zeppelin Store</a>
+    <form class="search-form">
+        <input class="search-input" type="search" placeholder="Search" aria-label="Search">
+        <button class="search-button" type="submit">Search</button>
+    </form>
+</div>
+
+<div class="container">
+    <h1 class="rock-title">Catálogo de Productos</h1>
+    <div class="row">
+        @foreach($producto as $index => $prod)
+            @if($index % 3 == 0)
+                </div><div class="row">
+            @endif
+            <div class="col-4">
+                <div class="card">
+                    <img class="card-img-top" src='{{ asset($prod->imagen) }}' alt="Card image cap">
+                    <div class="larger-image">
+                        <img src='{{ asset($prod->imagen) }}' alt="Larger Preview">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{$prod->nomproducto}}</h5>
+                        <p class="card-description card-text">{{$prod->descripcion}}</p>
+                        <p class="card-price card-text"><b>precio: $ </b>{{$prod->precio}}</p>
+                        <button>Agregar</button>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
+
+
+
+
+
